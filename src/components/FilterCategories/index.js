260bc8ef2686182cf933501e03d54d3fb67defcb
Carlos Quesada <carlosquesadaumana@gmail.com>
2022-01-10 11:23:07 -0600

    Filter Categories dynamic render added

--- src/components/FilterCategories/index.js
+++ src/components/FilterCategories/index.js
@@ -19,129 +19,88 @@ export const FilterCategories = () => {
       const handleSubmit = async (e) => {
             e.preventDefault();
 
-            console.log(form)
-
             const res = await fetch(`http://localhost:9000/products${formatUrlParams(form)}`)
             const data = await res.json()
 
             dispatch(setProducts(data));
       };
 
-      // const filterItems = [
-      //       {
-      //             id: 1,
-      //             name: 'Gender',
-      //             options: [{
-      //                               id: new Date().getTime(),
-      //                               title: 'All'
-      //                         }, {
-      //                               id: new Date().getTime(),
-      //                               title: 'Male'
-      //                         }, {
-      //                               id: new Date().getTime(),
-      //                               title: 'Female'
-      //                         }]
-      //       },
-      //       {
-      //             id: 2,
-      //             name: 'Type',
-      //             options: ['All', 'Shoe', 'Shirt']
-      //       },
-      //       {
-      //             id: 3,
-      //             name: 'Category',
-      //             options: ['All', 'Sport']
-      //       }
-      // ]
+      const filterItems = [
+            {
+                  id: 1,
+                  name: 'Gender',
+                  options: [{
+                                    id: 1,
+                                    title: 'All'
+                              }, {
+                                    id: 2,
+                                    title: 'Male'
+                              }, {
+                                    id: 3,
+                                    title: 'Female'
+                              }]
+            },
+            {
+                  id: 2,
+                  name: 'Type',
+                  options: [{
+                                    id: 1,
+                                    title: 'All'
+                              }, {
+                                    id: 2,
+                                    title: 'Shoe'
+                              }, {
+                                    id: 3,
+                                    title: 'Shirt'
+                              }]
+            },
+            {
+                  id: 3,
+                  name: 'Category',
+                  options: [{
+                                    id: 1,
+                                    title: 'All'
+                              }, {
+                                    id: 2,
+                                    title: 'Sport'
+                              }, {
+                                    id: 3,
+                                    title: 'Casual'
+                              }]
+            }
+      ]
 
       return (
             <form className="form-filter" onSubmit={handleSubmit}>
                   <h2>Filter</h2>
 
                   <div className="form-filter-groups">
-                        <div className="filter-group">
-                              <h3>Gender</h3>
-                              <select 
-                                    name="gender"
-                                    className="form-control"
-                                    onChange={handleInputChange}
-                              >
-                                    <option value="all">All</option>
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                              </select>
-                        </div>
-
-                        <div className="filter-group">
-                              <h3>Type</h3>
-                              <select 
-                                    name="type"
-                                    className="form-control"
-                                    onChange={handleInputChange}
-                              >
-                                    <option value="all">All</option>
-                                    <option value="shoe">Shoe</option>
-                                    <option value="shirt">Shirt</option>
-                              </select>
-                        </div>
-
-                        <div className="filter-group">
-                              <h3>Minimun Price</h3>
-                              <div className="form-group">
-                                    <input
-                                          type="number"
-                                          className="form-control"
-                                          name="price"
-                                          min="100"
-                                          max="1000"
-                                          onChange={handleInputChange}
-                                    />
-                                    
-                              </div>
-                        </div>
-
-                        <div className="filter-group">
-                              <h3>Category</h3>
-                              <select 
-                                    name="category"
-                                    className="form-control"
-                                    onChange={handleInputChange}
-                              >
-                                    <option value="all">All</option>
-                                    <option value="sport">Sport</option>
-                                    <option value="casual">Casual</option>
-                              </select>
-                        </div>
-
-                        {/* {
+                        {
                               filterItems.map(item => (
-                                    <div className="filter-group">
+                                    <div key={item.id} className="filter-group">
                                           <h3>{item.name}</h3>
 
                                           <select 
                                                 name={item.name.toLowerCase()} 
                                                 key={item.id}
+                                                className="form-control"
                                                 onChange={handleInputChange}
                                           >
-                                                {
-                                                      item.options.map(option => (
-                                                            <option 
-                                                                  key={new Date().getTime()}
-                                                                  value={option.title.toLowerCase()}
-                                                            >
-                                                                  { option }
-                                                            </option>
-                                                      ))
-                                                }
+                                             {
+                                                   item.options.map(option => (
+                                                         <option 
+                                                            key={option.id} 
+                                                            value={option.title.toLowerCase()}
+                                                      >
+                                                               { option.title }
+                                                         </option>
+                                                   ))
+                                             }
                                           </select>
                                     </div>
                               ))
-                        } */}
+                        }
                   </div>
-
-
-
-
 
                   <button className="btn" type='submit' >Filter</button>
             </form>
